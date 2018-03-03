36393a716778423ae3ff07d2624d0d5afce6d996
irfanbaigse <irfan.blackhawk@gmail.com>
2018-03-03 20:10:55 +0400

    init commit

--- config/database.php
+++ config/database.php
@@ -14,9 +14,22 @@ return [
 
             ],
         ],
+        'mysql'   => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'port'      => env('DB_PORT', 3306),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => env('DB_CHARSET', 'utf8'),
+            'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
+            'prefix'    => env('DB_PREFIX', ''),
+            'timezone'  => env('DB_TIMEZONE', '+00:00'),
+            'strict'    => env('DB_STRICT_MODE', false),
+        ],
     ],
     'migrations'  => 'migrations',
-    'redis' => [
+    'redis'       => [
         'cluster' => env('REDIS_CLUSTER', false),
         'default' => [
             'host'     => env('REDIS_HOST', '127.0.0.1'),
