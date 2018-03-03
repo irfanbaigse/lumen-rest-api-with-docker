481dcb559ebbfdb017aa7796c2fdce278e37662a
irfanbaigse <irfan.blackhawk@gmail.com>
2018-03-03 18:41:40 +0400

    init commit

--- config/database.php
+++ config/database.php
@@ -1,18 +1,28 @@
 <?php
 
 return [
-    'default' => 'mongodb',
+    'default'     => 'mongodb',
     'connections' => [
         'mongodb' => [
-            'driver'    => 'mongodb',
-            'host'      => env('MONGO_DB_HOST', 'localhost'),
-            'port'      => env('MONGO_DB_PORT', 27017),
-            'database'  => env('MONGO_DB_DATABASE', 'homestead'),
-            'username'  => env('MONGO_DB_USERNAME', ''),
-            'password'  => env('MONGO_DB_PASSWORD', ''),
-            'options' => [
+            'driver'   => 'mongodb',
+            'host'     => env('MONGO_DB_HOST', 'localhost'),
+            'port'     => env('MONGO_DB_PORT', 27017),
+            'database' => env('MONGO_DB_DATABASE', 'homestead'),
+            'username' => env('MONGO_DB_USERNAME', ''),
+            'password' => env('MONGO_DB_PASSWORD', ''),
+            'options'  => [
 
-            ]
+            ],
         ],
-    ]
+    ],
+    'migrations'  => 'migrations',
+    'redis' => [
+        'cluster' => env('REDIS_CLUSTER', false),
+        'default' => [
+            'host'     => env('REDIS_HOST', '127.0.0.1'),
+            'port'     => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DATABASE', 0),
+            'password' => env('REDIS_PASSWORD', null),
+        ],
+    ],
 ];
